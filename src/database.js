5fd5ffe5531c70f3cacd59f1563c2d53e93e6e8b
kevinospina03 <kevinospina03@gmail.com>
2019-10-15 01:41:50 -0500

    commit2

--- src/database.js
+++ src/database.js
@@ -5,6 +5,24 @@ const {database} = require('./keys')
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 
+//const uri = "mongodb+srv://kevin:<password>@cluster0-yrw8e.mongodb.net/test?retryWrites=true&w=majority";
+
+/*const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://kevin:<password>@cluster0-yrw8e.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});*/
+
+
+
 mongoose.connect('mongodb+srv://kevin:nacional3@cluster0-yrw8e.mongodb.net/test?retryWrites=true&w=majority')
+//mongoose.connect(database.URI)
     .then(db=> console.log('db connected'))
     .catch(err => console.error(err))
+
+
+    
+
