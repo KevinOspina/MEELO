3051d4a32e501628a767fed5368f5f31462fb73c
kevinospina03 <kevinospina03@gmail.com>
2019-10-15 01:24:59 -0500

    comit1.1

--- src/database.js
+++ src/database.js
@@ -5,6 +5,6 @@ const {database} = require('./keys')
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect(database.URI)
+mongoose.connect('mongodb+srv://kevin:nacional3@cluster0-yrw8e.mongodb.net/test?retryWrites=true&w=majority')
     .then(db=> console.log('db connected'))
     .catch(err => console.error(err))
